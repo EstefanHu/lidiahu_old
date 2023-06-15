584c46e242fd32511acb1affceee61f92648ce1f
Estefan Hu <ehu@goodwaygroup.com>
2023-06-14 23:26:51 -0700

    feat(hero): Made hero responsive

--- src/app/(landing)/(home)/page.tsx
+++ src/app/(landing)/(home)/page.tsx
@@ -1,9 +1,9 @@
-import styles from './home.module.scss';
+import hero from './hero.module.scss';
 
 export default function Home() {
   return (
     <>
-      <section className={styles.hero}>
+      <section className={hero.hero}>
         <h1>I will extend peace to her like a river</h1>
         <p>- Isaiah 66:12</p>
       </section>
