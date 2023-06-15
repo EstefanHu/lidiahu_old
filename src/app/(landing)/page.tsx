51dab9e70f40ba0e5e44b18be3d4f9ed3494cfe7
Estefan Hu <ehu@goodwaygroup.com>
2023-06-14 22:21:46 -0700

    feat(layout): Added layout

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>“I will extend peace to her like a river.”</h1>
+      </div>
+    </main>
+  );
+}
