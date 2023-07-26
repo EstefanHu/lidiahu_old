a991a0ed5ac277d9f7a982fd2c48359102f99941
Estefan Hu <estefanhu074@gmail.com>
2023-07-26 13:42:17 -0700

    corrected navigation behavior

--- src/app/(landing)/p/page.tsx
+++ src/app/(landing)/p/page.tsx
@@ -3,26 +3,24 @@ import styles from './post.module.scss';
 
 const POSTS = [
   {
-    title: 'extended peace',
-    url: '/extended-peace',
-    description: 'marie goes for a hike',
-    category: 'short story',
-    date: 'Mon Jun 19',
-  },
-  {
-    title: 'Weathered Basket',
-    url: '/weathered-basket',
+    title: 'weathered basket',
     description: 'add description/synopsis here',
     category: 'short story',
     date: 'Tue Jul 25',
+  },
+  {
+    title: 'extended peace',
+    description: 'marie goes for a hike',
+    category: 'short story',
+    date: 'Mon Jun 19',
   },
 ];
 
 export default function Posts() {
   return (
     <>
-      {POSTS.map(({ title, url, description, category, date }) => (
-        <Link key={title} href={`/p/${url}`}>
+      {POSTS.map(({ title, description, category, date }) => (
+        <Link key={title} href={`/p/${title.replace(' ', '-')}`}>
           <div className={styles.post}>
             <p className={styles.header}>
               <span>{category}</span> - {date}
