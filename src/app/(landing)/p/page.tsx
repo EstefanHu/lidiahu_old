6215f529292eaf7d63c7aef6a246d23a6c87f2fd
Estefan Hu <estefanhu074@gmail.com>
2023-07-26 13:26:37 -0700

    verse

--- src/app/(landing)/p/page.tsx
+++ src/app/(landing)/p/page.tsx
@@ -12,20 +12,24 @@ const POSTS = [
   {
     title: 'Weathered Basket',
     url: '/weathered-basket',
-
-  }
+    description: 'add description/synopsis here',
+    category: 'short story',
+    date: 'Tue Jul 25',
+  },
 ];
 
 export default function Posts() {
   return (
     <>
       {POSTS.map(({ title, url, description, category, date }) => (
-        <Link key={title} href={`/p/${url}`} className={styles.post}>
-          <p className={styles.header}>
-            <span>{category}</span> - {date}
-          </p>
-          <h3>{title}</h3>
-          <p>{description}</p>
+        <Link key={title} href={`/p/${url}`}>
+          <div className={styles.post}>
+            <p className={styles.header}>
+              <span>{category}</span> - {date}
+            </p>
+            <h3>{title}</h3>
+            <p>{description}</p>
+          </div>
         </Link>
       ))}
     </>
